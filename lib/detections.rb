2307288eebaf2bad0b3b9b0a10c8c447a39d0f8a
Álvaro Almeida Freire Stivi <alvaro.stivi@gmail.com>
2016-09-29 05:26:31 -0300

    Implements a test run

--- lib/detections.rb
+++ lib/detections.rb
@@ -1,12 +1,16 @@
 require_relative 'file_reader'
+require_relative 'hierarchical_cluster'
 
 class Detections
 
   def detect
     file = File.open('../fixtures/detections-first-1000.csv')
     file_reader = FileReader.new(file)
-    file_reader.read_detections.map do |a|
-      puts "[track_id: #{a.track_id}, audio_source_id: #{a.audio_source_id}]"
+    detections = file_reader.read_detections
+    puts 'File read!'
+    clusterizer = HierarchicalCluster.new(detections, 0.5)
+    clusterizer.clusterize.each do |a|
+      puts a.size
     end
   end
 end
