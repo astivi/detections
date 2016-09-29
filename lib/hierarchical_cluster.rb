20f25bd5285701245464c6eaffb9431748183610
Álvaro Almeida Freire Stivi <alvaro.stivi@gmail.com>
2016-09-29 05:10:36 -0300

    Adds threshold for comparison between clusters

--- lib/hierarchical_cluster.rb
+++ lib/hierarchical_cluster.rb
@@ -1,9 +1,10 @@
 #http://www.cs.utah.edu/~piyush/teaching/4-10-print.pdf
 class HierarchicalCluster
-  def initialize(detections)
+  def initialize(detections, threshold)
     @detections = detections.map do |detection|
       Set.new [detection]
     end
+    @threshold = threshold
   end
 
   def clusterize
@@ -25,7 +26,7 @@ class HierarchicalCluster
 
   def find_max_likeness
     max_i = max_j = nil
-    max_likeness = 0
+    max_likeness = @threshold
     (0..@detections.size-1).each do |i|
       (i+1..@detections.size-1).each do |j|
         likeness = calculate_likeness(@detections[i], @detections[j])
@@ -40,14 +41,15 @@ class HierarchicalCluster
   end
 
   def calculate_likeness(cluster1, cluster2)
-    alike = 0
-    cluster1.each do |i|
-      cluster2.each do |j|
-        if i.audio_source_id == j.audio_source_id or i.track_id == j.track_id
-          alike = alike + 2
+    alike = Array.new(cluster1.size + cluster2.size, 0)
+    cluster1.each_with_index do |c1, i|
+      cluster2.each_with_index do |c2, j|
+        if c1.audio_source_id == c2.audio_source_id or c1.track_id == c2.track_id
+          alike[i] = 1
+          alike[alike.size - j] = 1
         end
       end
     end
-    alike / (cluster1.size + cluster2.size)
+    (1.0 * alike.count(1)) / (cluster1.size + cluster2.size)
   end
 end
--- spec/hierarchical_cluster_spec.rb
+++ spec/hierarchical_cluster_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe HierarchicalCluster, '#clusterize' do
   context 'given two different audio sources' do
     it 'segregates into different genres' do
       detections = [TrackDetection.new(1, 1), TrackDetection.new(2, 2)]
-      cluster = HierarchicalCluster.new(detections)
+      cluster = HierarchicalCluster.new(detections, 0)
       expect(cluster.clusterize.size).to eq(2)
     end
   end
@@ -13,7 +13,7 @@ RSpec.describe HierarchicalCluster, '#clusterize' do
   context 'given the same audio source' do
     it 'aggregates into the same genre' do
       detections = [TrackDetection.new(1, 1), TrackDetection.new(1, 2)]
-      cluster = HierarchicalCluster.new(detections)
+      cluster = HierarchicalCluster.new(detections, 0)
       expect(cluster.clusterize.size).to eq(1)
     end
   end
@@ -21,7 +21,7 @@ RSpec.describe HierarchicalCluster, '#clusterize' do
   context 'given the same track' do
     it 'aggregates into the same genre' do
       detections = [TrackDetection.new(1, 1), TrackDetection.new(2, 1)]
-      cluster = HierarchicalCluster.new(detections)
+      cluster = HierarchicalCluster.new(detections, 0)
       expect(cluster.clusterize.size).to eq(1)
     end
   end
@@ -29,7 +29,7 @@ RSpec.describe HierarchicalCluster, '#clusterize' do
   context 'given four different detections with shared attributes' do
     it 'aggregates into the same genre' do
       detections = [TrackDetection.new(1, 1), TrackDetection.new(1, 2), TrackDetection.new(2, 1), TrackDetection.new(2, 2)]
-      cluster = HierarchicalCluster.new(detections)
+      cluster = HierarchicalCluster.new(detections, 0)
       expect(cluster.clusterize.size).to eq(1)
     end
   end
@@ -37,8 +37,24 @@ RSpec.describe HierarchicalCluster, '#clusterize' do
   context 'given four different detections with mixed attributes' do
     it 'aggregates into two genres' do
       detections = [TrackDetection.new(1, 1), TrackDetection.new(1, 2), TrackDetection.new(3, 4), TrackDetection.new(4, 4)]
-      cluster = HierarchicalCluster.new(detections)
+      cluster = HierarchicalCluster.new(detections, 0)
       expect(cluster.clusterize.size).to eq(2)
+    end
+  end
+
+  context 'given two clusters with little intersection and a high threshold' do
+    it 'segregates into two genres' do
+      detections = [TrackDetection.new(1, 1), TrackDetection.new(1, 2), TrackDetection.new(1, 3), TrackDetection.new(2, 4), TrackDetection.new(2, 5), TrackDetection.new(2, 6), TrackDetection.new(1, 6)]
+      cluster = HierarchicalCluster.new(detections, 0.66)
+      expect(cluster.clusterize.size).to eq(2)
+    end
+  end
+
+  context 'given two clusters with little intersection and a low threshold' do
+    it 'aggregate into one genre' do
+      detections = [TrackDetection.new(1, 1), TrackDetection.new(1, 2), TrackDetection.new(1, 3), TrackDetection.new(2, 4), TrackDetection.new(2, 5), TrackDetection.new(2, 6), TrackDetection.new(1, 6)]
+      cluster = HierarchicalCluster.new(detections, 0.33)
+      expect(cluster.clusterize.size).to eq(1)
     end
   end
 end
