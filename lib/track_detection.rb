73ea4cdcf196263094bbf6b2f9620395b6cfa37c
Álvaro Almeida Freire Stivi <alvaro.stivi@gmail.com>
2016-09-29 04:26:47 -0300

    Implements equals on TrackDetection

--- lib/track_detection.rb
+++ lib/track_detection.rb
@@ -4,4 +4,9 @@ class TrackDetection
     @track_id = track_id
   end
   attr_accessor :audio_source_id, :track_id
+
+  def ==(other)
+    return false unless other.instance_of? TrackDetection
+    @audio_source_id == other.audio_source_id && @track_id = other.track_id
+  end
 end
