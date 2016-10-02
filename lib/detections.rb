1a5a0f8fa8ad65aafd61d7495e35d200fb094cb4
Álvaro Stivi <alvaro.stivi@gmail.com>
2016-10-01 21:59:38 -0300

    Handles input param for file

--- lib/detections.rb
+++ lib/detections.rb
@@ -6,20 +6,30 @@ require_relative 'analysis/hierarchical_cluster'
 
 class Detections
 
-  def detect
-    input_file = File.open(File.expand_path('../../fixtures/detections-first-1000.csv', __FILE__))
+  def detect(input_file)
     file_reader = FileReader.new(input_file)
     detections = file_reader.read_detections
     clusterizer = HierarchicalCluster.new(detections, 0.5)
     classificator = ClusterClassificator.new(clusterizer.clusterize)
     classifications = classificator.classify
-    by_track_output_file = File.open(File.expand_path('../../output/detections-first-1000.by_track.csv', __FILE__), 'a')
+    basename = File.basename(input_file.path, '.csv')
+    by_track_output_file = File.open(File.expand_path("../../output/#{basename}.by_track", __FILE__), File::CREAT|File::TRUNC|File::RDWR)
     by_track_file_writer = ByTrackFileWriter.new(by_track_output_file)
     by_track_file_writer.write_genres(classifications)
-    by_source_output_file = File.open(File.expand_path('../../output/detections-first-1000.by_source.csv', __FILE__), 'a')
+    by_source_output_file = File.open(File.expand_path("../../output/#{basename}.by_source", __FILE__), File::CREAT|File::TRUNC|File::RDWR)
     by_source_file_writer = BySourceFileWriter.new(by_source_output_file)
     by_source_file_writer.write_genres(classifications)
   end
+
+  def run
+    if (ARGV.nil? or ARGV.size != 1)
+      puts 'Usage: ruby detections.rb <file>'
+      return 1
+    end
+    detect(File.open(ARGV[0]))
+  rescue
+    puts 'Input file could not be opened.'
+  end
 end
 
-Detections.new.detect
+Detections.new.run
